3cb400d30a3cd0f0222f7bc3c36dc60183fa9fde
Ochuko Ekrresa <kelvinekrresa@gmail.com>
2021-02-01 20:49:03 +0100

    Implemented infinite scrolling

--- src/app.js
+++ src/app.js
@@ -5,49 +5,104 @@ import relativeTime from 'dayjs/plugin/relativeTime';
 
 dayjs.extend(relativeTime);
 
-async function fetchJobs() {
-	const response = await fetch(
-		'https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json'
-	);
+let currentPage = 1;
+const jobsLoader = document.getElementById('jobs-loader');
+const jobsContainer = document.getElementById('jobs');
+const scrollTrigger = document.getElementById('infinite-scroll-trigger');
 
-	const jobs = await response.json();
-	return jobs;
+async function fetchAndDisplayJobs(url, search) {
+	try {
+		const jobs = await fetchJobs(url);
+		currentPage++;
+		const jobsFragment = processJobs(jobs, { search });
+		displayJobs(jobsFragment, search);
+	} catch (error) {
+		console.error(err.message);
+	}
 }
 
-fetchJobs()
-	.then(jobs => {
-		const jobsContainer = document.getElementById('jobs');
-		const jobsLoader = document.getElementById('jobs-loader');
-		const body = document.getElementById('homepage');
-		const jobTemplate = document.getElementById('job-template');
-		const jobsFragment = document.createDocumentFragment();
+async function fetchJobs(url) {
+	jobsLoader.classList.add('show');
+	const response = await fetch(url);
+	jobsLoader.classList.remove('show');
 
-		jobs.forEach(job => {
-			const clone = document.importNode(jobTemplate.content, true);
+	return await response.json();
+}
 
-			const jobDate = clone.querySelector('#job-date');
-			const jobType = clone.querySelector('#job-type');
-			const jobLink = clone.querySelector('.job-url');
+function processJobs(jobs, ...args) {
+	const jobTemplate = document.getElementById('job-template');
+	const jobsFragment = document.createDocumentFragment();
 
-			const cardTitle = clone.querySelector('.card__title');
-			const cardCompany = clone.querySelector('.card__company');
-			const cardImage = clone.querySelector('.card__tag');
-			const cardLocation = clone.querySelector('.card__location');
+	jobs.forEach(job => {
+		const clone = document.importNode(jobTemplate.content, true);
 
-			cardTitle.textContent = job.title;
-			cardCompany.textContent = job.company;
-			cardImage.src = job.company_logo;
-			cardLocation.textContent = job.location;
-			jobType.textContent = job.type;
-			jobDate.textContent = dayjs().to(dayjs(job.created_at));
-			jobLink.href = '/job.html?job_id=' + job.id;
+		const jobDate = clone.querySelector('#job-date');
+		const jobType = clone.querySelector('#job-type');
+		const jobLink = clone.querySelector('.job-url');
 
-			jobsFragment.appendChild(clone);
-		});
+		const cardTitle = clone.querySelector('.card__title');
+		const cardCompany = clone.querySelector('.card__company');
+		const cardImage = clone.querySelector('.card__tag');
+		const cardLocation = clone.querySelector('.card__location');
 
-		body.removeChild(jobsLoader);
-		jobsContainer.appendChild(jobsFragment);
-	})
-	.catch(err => {
-		console.log(err.message);
+		cardTitle.textContent = job.title;
+		cardCompany.textContent = job.company;
+		cardImage.src = job.company_logo;
+		cardLocation.textContent = job.location;
+		jobType.textContent = job.type;
+		jobDate.textContent = dayjs().to(dayjs(job.created_at));
+		jobLink.href = '/job.html?job_id=' + job.id;
+
+		jobsFragment.appendChild(clone);
 	});
+
+	return jobsFragment;
+}
+
+function displayJobs(jobsFragment, replace) {
+	if (replace) {
+		jobsContainer.replaceChild(jobsFragment);
+	} else {
+		jobsContainer.appendChild(jobsFragment);
+	}
+}
+
+const searchForm = document.getElementById('search-form');
+
+searchForm.addEventListener('submit', async function (evt) {
+	evt.preventDefault();
+
+	const url = new URL(
+		'https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json?page=1'
+	);
+
+	const form = new FormData(this);
+
+	for (const elem of form.keys()) {
+		if (form.get(elem)) {
+			if (elem === 'full_time') {
+				url.searchParams.set('full_time', true);
+			} else {
+				url.searchParams.set(elem, form.get(elem));
+			}
+		}
+	}
+
+	await fetchAndDisplayJobs(url.href, true);
+});
+
+let observer = new IntersectionObserver(
+	entries => {
+		entries.forEach(entry => {
+			if (entry.isIntersecting) {
+				fetchAndDisplayJobs(
+					`https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json?page=${currentPage}`,
+					false
+				);
+			}
+		});
+	},
+	{ threshold: 0.75 }
+);
+
+observer.observe(scrollTrigger);
